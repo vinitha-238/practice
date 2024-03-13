d78c395a5350766b2c414f1c1c181506d325bae9
vinitha-238 <132841994+vinitha-238@users.noreply.github.com>
2024-03-13 17:58:28 +0530

    Add files via upload

---
+++ user/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.insuagg1.repository;
+
+import com.insuagg1.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Integer> {
+}
